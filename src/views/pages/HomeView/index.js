66adb4e15e12310c68663dff847e2ce1dfd63e6b
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-04-30 21:09:35 +0530

    Redractor: Remove all the unnecessary components

--- src/views/pages/HomeView/index.js
+++ src/views/pages/HomeView/index.js
@@ -1,13 +1,7 @@
 import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import Page from 'src/components/Page';
-import { experience, mentors } from '../../../data/HomeViewData/HomeViewData';
-import Footer from '../common/Footer';
-import Team from './Team';
-import CTA from './CTA';
-import Events from './Events';
 import Hero from './Hero';
-import MentorExperience from './MentorExperience';
 
 const useStyles = makeStyles(() => ({
   root: {}
@@ -19,11 +13,7 @@ function HomeView() {
   return (
     <Page className={classes.root} title="Code for Cause">
       <Hero />
-      <MentorExperience experience={experience} />
-      <Events id="events" />
-      <Team id="team" mentors={mentors} />
-      <CTA id="actions" />
-      <Footer />
+      {/* <Footer /> */}
     </Page>
   );
 }
