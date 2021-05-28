a16af2c2af8176ad66ee59f9730fa0900cf93ea3
Abhinav Sinha <zeborg3@gmail.com>
2021-05-28 15:06:58 +0530

    Added 'Profile', 'ProfileEvents' and 'ProfilePublications' components; Made subtle fixes and changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,4 @@
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import DrawerLayout from 'src/layouts/DrawerLayout';
 import Badge from './Badge';
@@ -7,16 +6,7 @@ import ProfileInfo from './ProfileInfo'
 import ProfilePublications from './ProfilePublications';
 import ProfileEvents from './ProfileEvents';
 
-const useStyles = makeStyles(theme => ({
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default
-  }
-}));
-
 export default function Dashboard() {
-  const classes = useStyles();
-
   return (
     <DrawerLayout>
         <Box display="flex">
@@ -31,7 +21,7 @@ export default function Dashboard() {
             <Box flexGrow={1}>
                 <ProfileEvents />
             </Box>
-            <Box maxWidth="26em" minWidth="24em">
+            <Box maxWidth="260em" minWidth="24em">
                 <ProfilePublications />
             </Box>
         </Box>
