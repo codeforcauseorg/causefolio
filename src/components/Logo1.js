4b31e794de65aa3fc3f5acbd6b431709d38b35e3
Abhinav Sinha <zeborg3@gmail.com>
2021-06-03 00:38:15 +0530

    Added 'ProjectsCarousel' component; Fixed missing icons and logos

--- src/components/Logo1.js
+++ src/components/Logo1.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Logo(props) {
-  return <img alt="Logo" height="32px" src="/static/Logo1.svg" {...props} />;
+  return <img alt="Logo" height="32px" src="/static/logo/causefolio.svg" {...props} />;
 }
 
 export default Logo;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,39 +2,40 @@ import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import DrawerLayout from 'src/layouts/DrawerLayout';
 import Badge from './Badge';
-import ProfileInfo from './ProfileInfo';
+import ProfileInfo from './ProfileInfo'
 import ProfilePublications from './ProfilePublications';
 import ProfileEvents from './ProfileEvents';
 import CommitChart from './CommitChart';
+import ProjectsCarousel from './ProjectsCarousel'
 
 export default function Profile() {
   return (
     <DrawerLayout>
-      <Grid>
-        <ProfileInfo />
-      </Grid>
-      <Grid
-        container
-        style={{ marginBottom: '21px' }}
-      >
-        <Grid item sm={4} style={{ marginRight: '21px' }}>
-          <CommitChart />
+        <Grid>
+            <ProfileInfo />
         </Grid>
-        <Grid item sm>
-          <Badge />
+        <Grid
+            container
+            style={{ marginBottom: '21px' }}
+        >
+            <Grid item sm={4} style={{ marginRight: '21px' }}>
+                <CommitChart />
+            </Grid>
+            <Grid item sm>
+                <Badge />
+            </Grid>
         </Grid>
-      </Grid>
-      <Grid
-        container
-        style={{ marginBottom: '21px' }}
-      >
-        <Grid sm>
-          <ProfileEvents />
+        <Grid container>
+            <Grid sm>
+                <ProfileEvents />
+            </Grid>
+            <Grid sm>
+                <ProfilePublications />
+            </Grid>
         </Grid>
-        <Grid sm>
-          <ProfilePublications />
+        <Grid>
+            <ProjectsCarousel />
         </Grid>
-      </Grid>
     </DrawerLayout>
   );
 }
