0e8b5243003232204e11a29aabf4aec8f38a79e3
harkiratsm <71957674+harkiratsm@users.noreply.github.com>
2021-06-10 13:45:53 +0530

    Loader for profile Page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,11 +10,25 @@ import ProjectsCarousel from './ProjectsCarousel';
 
 import { firebase } from 'src/services/authService';
 import { useSelector } from 'react-redux';
+import '../assets/css/loader.css'
 
 export default function Profile() {
   const user = useSelector(state => state.account.user);
   const [myProfile, setMyProfile] = useState(null);
+  const [loading,setLoading]=useState(false);
 
+  const Loader=()=>{
+    return(
+      <div className="sampleContainer">
+          <div className="loader">
+              <span className="dot dot_1"></span>
+              <span className="dot dot_2"></span>
+              <span className="dot dot_3"></span>
+              <span className="dot dot_4"></span>
+          </div>
+      </div>
+    )
+  }
   useEffect(() => {
     if (user !== undefined) {
       let userId = user.uid;
@@ -31,7 +45,9 @@ export default function Profile() {
           } else {
             console.log('No such document!');
           }
+          setLoading(true)
         })
+        
         .catch(error => {
           console.log('Error getting document:', error);
         });
@@ -40,30 +56,32 @@ export default function Profile() {
 
   return (
     <DrawerLayout>
-      <Grid>
-        {myProfile !== null ? (
-          <ProfileInfo myProfile={myProfile} />
-        ) : (
-          <>No Data</>
-        )}
-      </Grid>
-      <Grid container spacing={2} style={{ marginBottom: '21px' }}>
-        <Grid item xs={12} sm={12} md={5} style={{ marginBottom: '21px' }}>
-          <CommitChart />
+      {loading ?<>
+        <Grid>
+          {myProfile !== null ? (
+            <ProfileInfo myProfile={myProfile} />
+          ) : (
+            <>No Data</>
+          )}
         </Grid>
-        <Grid item xs={12} sm={12} md={7}>
-          <Badge />
+        <Grid container spacing={2} style={{ marginBottom: '21px' }}>
+          <Grid item xs={12} sm={12} md={5} style={{ marginBottom: '21px' }}>
+            <CommitChart />
+          </Grid>
+          <Grid item xs={12} sm={12} md={7}>
+            <Badge />
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container>
-        <Grid xs={12} sm={6}>
-          <ProfileEvents />
+        <Grid container>
+          <Grid xs={12} sm={6}>
+            <ProfileEvents />
+          </Grid>
+          <Grid xs={12} sm={6}>
+            <ProfilePublications />
+          </Grid>
         </Grid>
-        <Grid xs={12} sm={6}>
-          <ProfilePublications />
-        </Grid>
-      </Grid>
-      <ProjectsCarousel />
+        <ProjectsCarousel />
+      </>:<Loader></Loader>}
     </DrawerLayout>
   );
 }
