39a855749b48fbc352c44a169d9daeb0612d5c34
Amisha Kumari <amishakumari544@gmail.com>
2021-06-11 08:51:45 +0530

    Validation Added

--- src/components/CreateNewEvent.js
+++ src/components/CreateNewEvent.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import DrawerLayout from 'src/layouts/DrawerLayout';
+import {useForm } from "react-hook-form";
 import {
   Button,
-  Card,
   Grid,
   Box,
   Typography,
   InputBase,
   TextField
 } from '@material-ui/core';
+import ImageUploader from 'react-images-upload';
+
+
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -18,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     borderRadius: '20px',
     fontSize: '16px'
   },
-  social: {
+  speakerInput: {
     borderBottom: '1.3px solid #291757',
     marginBottom: '10px'
   },
@@ -43,11 +46,11 @@ const useStyles = makeStyles(theme => ({
   },
   createbtn: {
     marginTop: '60px',
-    marginLeft: '40px', 
+    marginLeft: '40px',
     [theme.breakpoints.down('md')]: {
       display: 'flex',
-      flexDirection: 'row-reverse',
-     }
+      flexDirection: 'row-reverse'
+    }
   },
   addbtn: {
     background: '#291757',
@@ -57,37 +60,12 @@ const useStyles = makeStyles(theme => ({
     borderRadius: '20px',
     marginRight: '60px'
   },
-  inputDiv: {
-    background: 'rgba(42, 23, 89, 0.25)',
-    borderRadius: '17px',
-    width: '451px',
-    marginBottom: '10px',
-    height: '70px'
-  },
-  input1: {
-    border: '0',
-    // padding: '10px',
-    font: 'inherit',
-    width: '100%',
-    backgroundColor: '#CCD2E3',
-    borderRadius: '20px',
-    outline: '0',
-    borderBlockColor: 'green',
-    borderColor: 'green',
-    '& .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#F2F7FF',
-      borderRadius: '20px',
-    [theme.breakpoints.down('md')]: {
-      width: '50%',
-    }
-  }
-  },
   image: {
     marginLeft: '71px',
     width: '350px',
     height: '183px',
     marginTop: '55px',
-    cursor:'pointer',
+    cursor: 'pointer',
     background: '#473672',
     borderRadius: '40px',
     [theme.breakpoints.down('xs')]: {
@@ -124,8 +102,8 @@ const useStyles = makeStyles(theme => ({
     color: '#fff',
     marginLeft: '70px',
     [theme.breakpoints.down('xs')]: {
-     fontSize:'18px',
-     marginLeft: '30px',
+      fontSize: '18px',
+      marginLeft: '30px'
     }
   },
   gallery: {
@@ -152,19 +130,36 @@ const useStyles = makeStyles(theme => ({
   date: {
     marginBottom: '16px',
     backgroundColor: '#CCD2E3',
-   
     borderRadius: '20px',
-    padding: "12.5px 14px",
+    padding: '12.5px 14px',
     '&:focus': {
       outline: 'none'
     },
-    
+
     '& .MuiOutlinedInput-notchedOutline': {
       // borderColor: '#F2F7FF',
-      borderRadius: '20px',
+      borderRadius: '20px'
     },
-    [theme.breakpoints.down('md')]:{
-      width: '100%',
+    [theme.breakpoints.down('md')]: {
+      width: '100%'
+    }
+  },
+  input1: {
+    border: '0',
+    marginBottom:'15px',
+    font: 'inherit',
+    width: '100%',
+    backgroundColor: '#CCD2E3',
+    borderRadius: '20px',
+    outline: '0',
+    borderBlockColor: 'green',
+    borderColor: 'green',
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#F2F7FF',
+      borderRadius: '20px',
+      [theme.breakpoints.down('md')]: {
+        width: '50%'
+      }
     }
   },
   textField: {
@@ -180,7 +175,19 @@ const useStyles = makeStyles(theme => ({
     }
   },
   paddingRight: {
-    paddingRight: '80px'
+    paddingRight: '80px',
+    marginLeft: '40px'
+  },
+ 
+
+  imagePreview: {
+    '& .uploadPictureContainer': {
+      width: '60%'
+    }
+  },
+  red:{
+    color: 'red',
+    marginBottom:'4px',
   }
 }));
 
@@ -188,15 +195,23 @@ function CreateNewEvent() {
   const classes = useStyles();
 
   // file upload
- 
+
   const handleChange = e => {
     // let selectedFile = e.target.files[0];
+  };
 
+  function onDrop(picture) {
+    console.log(picture);
   }
+
+  const { register, handleSubmit, control, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
 
   useEffect(() => {}, []);
   return (
+
     <DrawerLayout>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.root}>
         <Box display="flex" style={{ width: '100%' }}>
           <Box flexGrow={1}>
@@ -215,105 +230,172 @@ function CreateNewEvent() {
                   fullWidth
                   name="eventName"
                   type="eventName"
+                  control={control}
                   variant="outlined"
                   onChange={handleChange}
-                />
-
+                  {...register("eventName", {required: true,minLength:2,
+                    maxLength: 20,
+                    pattern: /^[A-Za-z]+$/i
+                  })} 
+                  />
+                    {errors?.eventName?.type === "required" && <p className={classes.red}>This field is required</p>}
+                    {errors?.eventName?.type === "maxLength" && (
+                    <p className={classes.red}>First name cannot exceed 20 characters</p>
+                  )}
+                  {errors?.eventName?.type === "pattern" && (
+                    <p className={classes.red}>Alphabetical characters only</p>
+                  )}
+     
                 <TextField
                   placeholder="Add Description of the event"
                   className={classes.textField}
                   multiline
+                  control={control}
                   fullWidth
-                  name="email"
-                  type="email"
+                  name="eventDescription"
+                  type="text"
                   variant="outlined"
                   onChange={handleChange}
-                />
+                  {...register("eventDescription", { required: true , maxLength: 60 })}
+                  />
+                    {errors.eventDescription && (
+                <p className={classes.red}>
+                  This field is required
+                </p>
+              )}
 
                 <Grid container>
                   <Grid item xs={12} sm={12} md={6}>
                     <TextField
                       id="date"
                       type="date"
+                      control={control}
+                      name="eventDate"
                       defaultValue="2017-05-24"
                       className={classes.date}
                       InputLabelProps={{
                         shrink: true
                       }}
                       style={{ marginRight: '30px' }}
-                    />
+                      {...register("eventDate", { required: true })}
+                      />
+                        {errors.eventDate && (
+                    <p className={classes.red}>
+                      This field is required
+                    </p>
+                  )}
                   </Grid>
                   <Grid item xs={12} sm={12} md={6}>
                     <TextField
                       fullWidth
+                      control={control}
                       className={classes.input1}
                       placeholder="Event Time"
-                      name="time"
+                      name="eventTime"
                       type="time"
                       id="time"
                       defaultValue="07:30"
                       variant="outlined"
                       onChange={handleChange}
-                    />
+                      {...register("eventTime", { required: true })}
+                      />
+                        {errors.eventTime && (
+                    <p className={classes.red}>
+                      This field is required
+                    </p>
+                  )}
                   </Grid>
                 </Grid>
 
                 <TextField
                   fullWidth
+                  control={control}
                   className={classes.textField}
                   placeholder="Event link / Registration link"
-                  name="link"
+                  name="eventLink"
                   type="link"
                   variant="outlined"
                   onChange={handleChange}
-                />
+                  {...register("eventLink", { required: true })}
+                  />
+                    {errors.eventLink && (
+                <p className={classes.red}>
+                  This field is required
+                </p>
+              )}
                 <fieldset className={classes.socialLinks}>
                   <InputBase
-                    className={classes.social}
+                   control={control}
+                    className={classes.speakerInput}
                     fullWidth
                     placeholder="Speaker Name"
-                  />
+                    name="speakerName"
+                    {...register("speakerName", { required: true ,minLength:2,
+                      maxLength: 20,
+                      pattern: /^[A-Za-z]+$/i
+                    })} 
+                    />
+                      {errors?.eventName?.type === "required" && <p className={classes.red}>This field is required</p>}
+                      {errors?.eventName?.type === "maxLength" && (
+                      <p className={classes.red}>Speaker name cannot exceed 20 characters</p>
+                    )}
+                    {errors?.eventName?.type === "pattern" && (
+                      <p className={classes.red}>Alphabetical characters only</p>
+                    )}
+       
                   <InputBase
-                    className={classes.social}
+                    className={classes.speakerInput}
                     fullWidth
+                    control={control}
+                    name="eventProfileLink"
                     placeholder="Speaker LinkedIn Profile Link"
-                  />
+                    {...register("eventProfileLink", { required: true })}
+                    />
+                      {errors.eventProfileLink && (
+                  <p className={classes.red}>
+                    This field is required
+                  </p>
+                )}
                 </fieldset>
-
-                <Button className={classes.button}>Add Speaker</Button>
-                {/* </div> */}
-              </Grid>
+                 <Button  className={classes.button}>Add Speaker</Button>
+             </Grid>
             </Grid>
             <div className={classes.createbtn}>
               <Button className={classes.cancelbtn}>Cancel</Button>
-              <Button className={classes.addbtn}>Create</Button>
+              <Button type="submit" className={classes.addbtn}>Create</Button>
             </div>
           </Box>
           <Box maxWidth="28em" minWidth="24em" className={classes.paddingRight}>
-            <label htmlFor="add_banner_image">
-              <Card className={classes.image}>
-                <img
-                  src="/static/images/gallery.svg"
-                  alt="gallery-icon"
-                  className={classes.gallery}
-                />
-                <h3 className={classes.text}>Add a banner image</h3>
-              </Card>
-            </label>
-            <input
-              type="file"
-              name="add_banner_image"
-              id="add_banner_image"
-              style={{ display: 'none' }}
-            />
+            <ImageUploader
+              withIcon={true}
+              buttonText="Choose image"
+              onChange={onDrop}
+              withPreview
+              singleImage
+              imgExtension={['.jpg', '.gif', '.png', '.gif']}
+              maxFileSize={5242880}
+              fileContainerStyle={{
+                boxShadow: '2px 2px 3px 1px rgb(0, 0, 0, 0.5)'
+              }}
+              className={classes.imagePreview}
+              {...register("eventLink", { required: true })}
+              />
+                {errors.eventLink && (
+            <p className={classes.red}>
+              Please add a Image
+            </p>
+          )}
             <img
               src="/static/images/event_img.svg"
               alt="gallery-icon"
-              style={{ marginLeft: '51px', marginTop: '20px' }}
-            />
+              name="galleryIcon"
+              style={{ marginLeft: '10px', marginTop: '20px' }}
+              />
+           
           </Box>
         </Box>
       </div>
+      </form>
     </DrawerLayout>
   );
 }
