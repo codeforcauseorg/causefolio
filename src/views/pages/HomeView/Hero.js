f13be1456c41100d6703e779af01e75dd571e21b
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-06-13 14:26:26 +0530

    Fix homepage responsiveness

--- src/views/pages/HomeView/Hero.js
+++ src/views/pages/HomeView/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+
 import {
   Box,
   Button,
   Container,
   Grid,
+  Hidden,
   Typography,
   makeStyles
 } from '@material-ui/core';
@@ -16,23 +18,39 @@ const useStyles = makeStyles(theme => ({
     paddingTop: 80,
     paddingBottom: 60,
     color: '#FFFFFF',
-    height: '90vh',
+    paddingLeft: 60,
+    paddingRight: 60,
+
+    height: '90.3vh',
     [theme.breakpoints.down('md')]: {
-      paddingTop: 40,
-      paddingBottom: 60
+      paddingTop: 15,
+      paddingBottom: 15
+    },
+    [theme.breakpoints.down('sm')]: {
+      height: 'auto'
     }
   },
   extraPadding: {
-    textAlign: 'justify'
+    padding: '0 70px 0px 0px',
+    textAlign: 'justify',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0'
+    }
   },
   image: {
     perspectiveOrigin: 'left center',
     transformStyle: 'preserve-3d',
     perspective: 1500,
     '& > img': {
-      maxWidth: '100%',
       height: 'auto',
       backfaceVisibility: 'hidden'
+    },
+    [theme.breakpoints.down('md')]: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      justifyContent: 'center'
     }
   },
   hide: {
@@ -50,9 +68,9 @@ function Hero({ className, ...rest }) {
     <div className={clsx(classes.root, className)} {...rest}>
       <Container
         maxWidth="lg"
-        style={{ paddingTop: '40px', position: 'relative' }}
+        style={{ padding: '40px 0px 0px', position: 'relative' }}
       >
-        <img
+        {/* <img
           src="/static/home/blob.svg"
           alt="blob"
           style={{
@@ -60,9 +78,34 @@ function Hero({ className, ...rest }) {
             top: '-20%',
             left: '-20%'
           }}
-        />
-        <Grid container spacing={0}>
-          <Grid item xs={10} md={5} style={{ zIndex: '20' }}>
+        /> */}
+        <Hidden mdDown>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            style={{
+              position: 'absolute',
+              top: '-35%',
+              left: '-15%',
+              width: '200%'
+            }}
+          >
+            <Box>
+              <div className={classes.image}>
+                <img
+                  src="/static/home/blob.svg"
+                  alt="blob"
+                  style={{
+                    width: '120%'
+                  }}
+                />
+              </div>
+            </Box>
+          </Grid>
+        </Hidden>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} style={{ zIndex: '20' }}>
             <Box
               display="flex"
               flexDirection="column"
@@ -81,7 +124,20 @@ function Hero({ className, ...rest }) {
               >
                 Code for Cause
               </Typography>
-              <Box mt={2}>
+              <Hidden mdUp>
+                <Box mt={6} mb={2}>
+                  <div className={classes.image}>
+                    <img
+                      alt="codeforcauseimg"
+                      src="/static/home/illus-1.svg"
+                      style={{
+                        width: '75%'
+                      }}
+                    />
+                  </div>
+                </Box>
+              </Hidden>
+              <Box mt={5}>
                 <Typography variant="body1">
                   An initiative to contribute to Open Source
                   <br />
@@ -91,16 +147,11 @@ function Hero({ className, ...rest }) {
                   software field to students &amp; professionals.
                 </Typography>
               </Box>
-              <Box mt={2}>
-                <Grid container spacing={3}>
-                  <Grid item>
+              <Box mt={2} mb={3}>
+                <Grid container>
+                  <Grid item xs={12} md={12}>
                     <Typography variant="h1" color="secondary"></Typography>
-                    <Box
-                      mt={2}
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
+                    <Box>
                       <Button
                         style={{
                           backgroundColor: '#ffffff',
@@ -122,20 +173,21 @@ function Hero({ className, ...rest }) {
               </Box>
             </Box>
           </Grid>
-
-          <Grid item xs={14} md={7}>
-            <Box position="relative">
-              <div className={classes.image}>
-                <img
-                  alt="codeforcauseimg"
-                  src="/static/home/illus-1.svg"
-                  style={{
-                    width: '100%'
-                  }}
-                />
-              </div>
-            </Box>
-          </Grid>
+          <Hidden smDown>
+            <Grid item xs={12} md={6}>
+              <Box>
+                <div className={classes.image}>
+                  <img
+                    alt="codeforcauseimg"
+                    src="/static/home/illus-1.svg"
+                    style={{
+                      width: '100%'
+                    }}
+                  />
+                </div>
+              </Box>
+            </Grid>
+          </Hidden>
         </Grid>
       </Container>
     </div>
