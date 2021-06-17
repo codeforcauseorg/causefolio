00a5daf5d57e6b7bb36897dc4cc4528d46288ed7
Amisha Kumari <amishakumari544@gmail.com>
2021-06-17 05:48:42 +0530

    changes

--- src/components/CreateNewEvent.js
+++ src/components/CreateNewEvent.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import DrawerLayout from 'src/layouts/DrawerLayout';
-import {
-  ValidatorForm,
-  TextValidator,
-} from 'react-material-ui-form-validator';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 // import CircularProgress from '@material-ui/core/CircularProgress';
 import {
   Button,
@@ -33,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '16px',
     marginTop: '8px',
     backgroundColor: 'rgb(232, 240, 254)',
-    border:'0',
+    border: '0',
     borderRadius: '20px',
     padding: '20px',
     '& .MuiOutlinedInput-notchedOutline': {
@@ -192,7 +189,7 @@ const useStyles = makeStyles((theme) => ({
 function CreateNewEvent() {
   const classes = useStyles();
   const history = useHistory();
-  const user = useSelector((state) => state.account.user);
+  const user = useSelector(state => state.account.user);
   const [speaker, setSpeaker] = useState([{}]);
   const [imageURL, setImageURL] = useState('');
   const initialFieldValues = {
@@ -202,17 +199,17 @@ function CreateNewEvent() {
     time: '',
     eventLink: '',
     speakerName: '',
-    speakerLinkedIn: '',
+    speakerLinkedIn: ''
   };
   const [formData, setFormData] = useState(initialFieldValues);
   // const [submit, setsubmit] = useState(0);
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSpeakerChange = (e) => {
-    let { id, name, value } = e.target;
+  const handleSpeakerChange = e => {
+    const { id, name, value } = e.target;
     let s = [...speaker];
     s[parseInt(id)][name] = value;
     setSpeaker(s);
@@ -227,7 +224,7 @@ function CreateNewEvent() {
     setSpeaker([...speaker]);
   };
 
-  const onDrop = async (picture) => {
+  const onDrop = async picture => {
     if (picture.length === 0 || user === null || user === undefined) return;
     // For the Loader
     setImageURL(null);
@@ -249,189 +246,193 @@ function CreateNewEvent() {
     const db = firebase.firestore();
     const ref = db.collection('events');
 
-    ref
-      .add(formData)
-      .then((docRef) => {
-        setFormData(initialFieldValues);
-        setSpeaker([{}]);
-        history.push(`/events/${docRef.id}`);
-      });
+    ref.add(formData).then(docRef => {
+      setFormData(initialFieldValues);
+      setSpeaker([{}]);
+      history.push(`/events/${docRef.id}`);
+    });
   };
   // const handleSubmit = () =>{
   //   console.log('submit');
   // }
 
   return (
-        <DrawerLayout>
-    <div className={classes.root}>
-    <ValidatorForm onSubmit={handleSubmit} onError={errors => console.log(errors)}>
-        <Box display="flex" style={{ width: '100%' }}>
-          <Box flexGrow={1}>
-            <Grid container>
-              <Grid item className={classes.topContainer}>
-                <Typography variant="h1" className={classes.topText}>
-                  New Event
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid container className={classes.event}>
-              <Grid style={{ width: '100%' }}>
-                <TextValidator
-                  placeholder="Enter name of the event"
-                  className={classes.textField}
-                  fullWidth
-                  name="eventName"
-                  value={formData.eventName}
-                  // variant="outlined"
-                  onChange={handleChange}
-                  validators={['required']}
-                  errorMessages={['This is a required field']}
-                />
-
-                <TextValidator
-                 placeholder="Add Description of the event"
-                  className={classes.textField}
-                  multiline
-                  rows={4}
-                  fullWidth
-                  name="description"
-                  value={formData.description}
-                  // variant="outlined"
-                  onChange={handleChange}
-                  validators={['required']}
-                  errorMessages={['This is a required field']}
-                />
-
-                <Grid container>
-                  <Grid item xs={12} sm={12} md={6}>
-                    <TextValidator
-                      id="date"
-                      type="date"
-                      defaultValue="2017-05-24"
-                      name="date"
-                      value={formData.date}
-                      className={classes.date}
-                      onChange={handleChange}
-                      InputLabelProps={{
-                        shrink: true
-                      }}
-                      style={{ marginRight: '30px' }}
-                      validators={['required']}
-                      errorMessages={['This is a required field']}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={12} md={6}>
-                    <TextValidator
-                      fullWidth
-                      className={classes.input1}
-                      type="time"
-                      name="time"
-                      value={formData.time}
-                      id="time"
-                      defaultValue="07:30"
-                      // variant="outlined"
-                      onChange={handleChange}
-                      validators={['required']}
-                      errorMessages={['This is a required field']}
-                    />
-                  </Grid>
+    <DrawerLayout>
+      <div className={classes.root}>
+        <ValidatorForm
+          onSubmit={handleSubmit}
+          onError={errors => console.log(errors)}
+        >
+          <Box display="flex" style={{ width: '100%' }}>
+            <Box flexGrow={1}>
+              <Grid container>
+                <Grid item className={classes.topContainer}>
+                  <Typography variant="h1" className={classes.topText}>
+                    New Event
+                  </Typography>
                 </Grid>
-
-                <TextValidator
-                  fullWidth
-                  className={classes.textField}
-                  placeholder="Event link / Registration link"
-                  name="eventLink"
-                  value={formData.eventLink}
-                  // variant="outlined"
-                  onChange={handleChange}
-                  validators={[
-                    'required']}
-                  errorMessages={[
-                    'This is a required field',
-                    'Please enter a valid contact number'
-                  ]}
-                />
-                {speaker.map((item, idx) => (
-                  <fieldset className={classes.speakerInput}>
-                    <TextValidator
-                      className={classes.speaker}
-                      fullWidth
-                      id={idx.toString()}
-                      name="speakerName"
-                      value={item.speakerName}
-                      placeholder="Speaker Name"
-                      onChange={handleSpeakerChange}
-                      validators={['required']}
-                      errorMessages={['This is a required field']}
-                    />
-                    <TextValidator
-                     key="linkedIn"
-                      className={classes.speaker}
-                      fullWidth
-                      id={idx.toString()}
-                      name="speakerLinkedIn"
-                      value={item.speakerLinkedIn}
-                      placeholder="Speaker LinkedIn Profile Link"
-                      onChange={handleSpeakerChange}
-                      validators={[
-                        'required',
-                        'matchRegexp:^(http(s)?://)?([w]+.)?linkedin.com/(pub|in|profile)'
-                      ]}
-                      errorMessages={[
-                        'This is a required field',
-                        'Please enter a valid URL'
-                      ]}
-                    />
-                  </fieldset>
-                 ))} 
-
-                {speaker.length > 1 && (
-                  <Button className={classes.button} onClick={removeSpeaker}>
-                    Remove Speaker
-                  </Button>
-                )}
-                <Button className={classes.button} onClick={addSpeaker}>
-                  Add Speaker
-                </Button>
               </Grid>
-            </Grid>
-            <div className={classes.createbtn}>
-              <Button
-                className={classes.cancelbtn}
-                onClick={() => history.push('/events')}
-              >
-                Cancel
-              </Button>
-              <Button className={classes.addbtn} type="submit">
-                Create
-              </Button>
-            </div>
+              <Grid container className={classes.event}>
+                <Grid style={{ width: '100%' }}>
+                  <TextValidator
+                    placeholder="Enter name of the event"
+                    className={classes.textField}
+                    fullWidth
+                    name="eventName"
+                    value={formData.eventName}
+                    // variant="outlined"
+                    onChange={handleChange}
+                    validators={['required']}
+                    errorMessages={['This is a required field']}
+                  />
+
+                  <TextValidator
+                    placeholder="Add Description of the event"
+                    className={classes.textField}
+                    multiline
+                    rows={4}
+                    fullWidth
+                    name="description"
+                    value={formData.description}
+                    // variant="outlined"
+                    onChange={handleChange}
+                    validators={['required']}
+                    errorMessages={['This is a required field']}
+                  />
+
+                  <Grid container>
+                    <Grid item xs={12} sm={12} md={6}>
+                      <TextValidator
+                        id="date"
+                        type="date"
+                        defaultValue="2017-05-24"
+                        name="date"
+                        value={formData.date}
+                        className={classes.date}
+                        onChange={handleChange}
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        style={{ marginRight: '30px' }}
+                        validators={['required']}
+                        errorMessages={['This is a required field']}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}>
+                      <TextValidator
+                        fullWidth
+                        className={classes.input1}
+                        type="time"
+                        name="time"
+                        value={formData.time}
+                        id="time"
+                        defaultValue="07:30"
+                        // variant="outlined"
+                        onChange={handleChange}
+                        validators={['required']}
+                        errorMessages={['This is a required field']}
+                      />
+                    </Grid>
+                  </Grid>
+
+                  <TextValidator
+                    fullWidth
+                    className={classes.textField}
+                    placeholder="Event link / Registration link"
+                    name="eventLink"
+                    value={formData.eventLink}
+                    // variant="outlined"
+                    onChange={handleChange}
+                    validators={['required']}
+                    errorMessages={[
+                      'This is a required field',
+                      'Please enter a valid contact number'
+                    ]}
+                  />
+                  {speaker.map((item, idx) => (
+                    <fieldset className={classes.speakerInput}>
+                      <TextValidator
+                        className={classes.speaker}
+                        fullWidth
+                        id={idx.toString()}
+                        name="speakerName"
+                        value={item.speakerName}
+                        placeholder="Speaker Name"
+                        onChange={handleSpeakerChange}
+                        validators={['required']}
+                        errorMessages={['This is a required field']}
+                      />
+                      <TextValidator
+                        key="linkedIn"
+                        className={classes.speaker}
+                        fullWidth
+                        id={idx.toString()}
+                        name="speakerLinkedIn"
+                        value={item.speakerLinkedIn}
+                        placeholder="Speaker LinkedIn Profile Link"
+                        onChange={handleSpeakerChange}
+                        validators={[
+                          'required',
+                          'matchRegexp:^(http(s)?://)?([w]+.)?linkedin.com/(pub|in|profile)'
+                        ]}
+                        errorMessages={[
+                          'This is a required field',
+                          'Please enter a valid URL'
+                        ]}
+                      />
+                    </fieldset>
+                  ))}
+
+                  {speaker.length > 1 && (
+                    <Button className={classes.button} onClick={removeSpeaker}>
+                      Remove Speaker
+                    </Button>
+                  )}
+                  <Button className={classes.button} onClick={addSpeaker}>
+                    Add Speaker
+                  </Button>
+                </Grid>
+              </Grid>
+              <div className={classes.createbtn}>
+                <Button
+                  className={classes.cancelbtn}
+                  onClick={() => history.push('/events')}
+                >
+                  Cancel
+                </Button>
+                <Button className={classes.addbtn} type="submit">
+                  Create
+                </Button>
+              </div>
+            </Box>
+            <Box
+              maxWidth="28em"
+              minWidth="24em"
+              className={classes.paddingRight}
+            >
+              <ImageUploader
+                withIcon
+                buttonText="Choose image"
+                onChange={onDrop}
+                withPreview={imageURL !== null}
+                singleImage
+                imgExtension={['.jpg', '.gif', '.png']}
+                maxFileSize={5242880}
+                fileContainerStyle={{
+                  boxShadow: '2px 2px 3px 1px rgb(0, 0, 0, 0.5)'
+                }}
+                className={classes.imagePreview}
+              />
+              {imageURL === null && <LinearProgress aria-label="Uploading" />}
+              <img
+                src="/static/images/event_img.svg"
+                alt="gallery-icon"
+                style={{ marginLeft: '10px', marginTop: '20px' }}
+              />
+            </Box>
           </Box>
-          <Box maxWidth="28em" minWidth="24em" className={classes.paddingRight}>
-            <ImageUploader
-              withIcon
-              buttonText="Choose image"
-              onChange={onDrop}
-              withPreview={imageURL !== null}
-              singleImage
-              imgExtension={['.jpg', '.gif', '.png']}
-              maxFileSize={5242880}
-              fileContainerStyle={{
-                boxShadow: '2px 2px 3px 1px rgb(0, 0, 0, 0.5)'
-              }}
-              className={classes.imagePreview}
-            />
-            {imageURL === null && <LinearProgress aria-label="Uploading" />}
-            <img
-              src="/static/images/event_img.svg"
-              alt="gallery-icon"
-              style={{ marginLeft: '10px', marginTop: '20px' }}
-            />
-          </Box>
-   </Box>
-    </ValidatorForm>
-    </div>
+        </ValidatorForm>
+      </div>
     </DrawerLayout>
   );
 }
