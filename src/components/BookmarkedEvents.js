6bf1ce577b28d5c6a584a4383c9aa804e60c9ef5
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-06-26 21:37:04 +0530

    Add bookmark feature

--- src/components/BookmarkedEvents.js
+++ src/components/BookmarkedEvents.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Button, Grid, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { firebase } from 'src/services/authService';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -54,7 +56,36 @@ const useStyles = makeStyles(() => ({
 
 function BookmarkedEvents() {
   const classes = useStyles();
-  useEffect(() => {}, []);
+  const user = useSelector(state => state.account.user);
+  const [bookmarkEvent, setBookmarkEvent] = useState([]);
+  const [eventID, setEventID] = useState(null);
+
+  const readIds = async (collection, ids) => {
+    setEventID(ids);
+    const reads = ids.map(id => collection.doc(id).get());
+    const result = await Promise.all(reads);
+    return result.map(r => r.data());
+  };
+
+  useEffect(() => {
+    const fetchBookmarkEvents = async () => {
+      if (user === undefined || user === null) return;
+
+      const userID = user.uid;
+      const db = firebase.firestore();
+      const userRef = await db.collection('users').doc(userID);
+      userRef.get().then(doc => {
+        if (doc.exists) {
+          let data = doc.data();
+          readIds(db.collection('events'), data.bookmarked).then(result =>
+            setBookmarkEvent(result)
+          );
+        }
+      });
+    };
+    fetchBookmarkEvents();
+  }, [user]);
+
   return (
     <div className={classes.root}>
       <Grid container>
@@ -77,134 +108,42 @@ function BookmarkedEvents() {
       </Grid>
       <Grid container className={classes.bmEvents}>
         {/* sample bookmarked events */}
-        <Grid className={classes.bmText}>
-          <Grid
-            container
-            style={{
-              backgroundColor: '#291757CC',
-              width: '114px',
-              height: '147px',
-              borderRadius: '20px'
-            }}
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Typography variant="h5" style={{ marginBottom: '14px' }}>
-              Event-1
-            </Typography>
-            <div style={{ display: 'inline-block', margin: '0 auto' }}>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                29TH MARCH
-              </Typography>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                6:00 PM
-              </Typography>
-            </div>
-            <Button
-              style={{ backgroundColor: 'white' }}
-              className={classes.checkOut}
-            >
-              CHECK OUT
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid className={classes.bmText}>
-          <Grid
-            container
-            style={{
-              backgroundColor: '#291757CC',
-              width: '114px',
-              height: '147px',
-              borderRadius: '20px'
-            }}
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Typography variant="h5" style={{ marginBottom: '14px' }}>
-              Event-2
-            </Typography>
-            <div style={{ display: 'inline-block', margin: '0 auto' }}>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                29TH MARCH
-              </Typography>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                6:00 PM
-              </Typography>
-            </div>
-            <Button
-              style={{ backgroundColor: 'white' }}
-              className={classes.checkOut}
-            >
-              CHECK OUT
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid className={classes.bmText}>
-          <Grid
-            container
-            style={{
-              backgroundColor: '#291757CC',
-              width: '114px',
-              height: '147px',
-              borderRadius: '20px'
-            }}
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Typography variant="h5" style={{ marginBottom: '14px' }}>
-              Event-3
-            </Typography>
-            <div style={{ display: 'inline-block', margin: '0 auto' }}>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                29TH MARCH
-              </Typography>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                6:00 PM
-              </Typography>
-            </div>
-            <Button
-              style={{ backgroundColor: 'white' }}
-              className={classes.checkOut}
-            >
-              CHECK OUT
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid className={classes.bmText}>
-          <Grid
-            container
-            style={{
-              backgroundColor: '#291757CC',
-              width: '114px',
-              height: '147px',
-              borderRadius: '20px'
-            }}
-            align="center"
-            justify="center"
-            direction="column"
-          >
-            <Typography variant="h5" style={{ marginBottom: '14px' }}>
-              Event-4
-            </Typography>
-            <div style={{ display: 'inline-block', margin: '0 auto' }}>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                29TH MARCH
-              </Typography>
-              <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
-                6:00 PM
-              </Typography>
-            </div>
-            <Button
-              style={{ backgroundColor: 'white' }}
-              className={classes.checkOut}
-            >
-              CHECK OUT
-            </Button>
-          </Grid>
-        </Grid>
+        {bookmarkEvent.length > 0 &&
+          bookmarkEvent.map((event, idx) => (
+            <Grid key={idx} className={classes.bmText}>
+              <Grid
+                container
+                style={{
+                  backgroundColor: '#291757CC',
+                  width: '114px',
+                  height: '147px',
+                  borderRadius: '20px'
+                }}
+                align="center"
+                justify="center"
+                direction="column"
+              >
+                <Typography variant="h5" style={{ marginBottom: '14px' }}>
+                  {event.eventName}
+                </Typography>
+                <div style={{ display: 'inline-block', margin: '0 auto' }}>
+                  <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
+                    {event.date}
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ fontSize: '10px' }}>
+                    {event.time}
+                  </Typography>
+                </div>
+                <Button
+                  style={{ backgroundColor: 'white' }}
+                  className={classes.checkOut}
+                  href={`/events/${eventID[idx]}`}
+                >
+                  CHECK OUT
+                </Button>
+              </Grid>
+            </Grid>
+          ))}
       </Grid>
     </div>
   );
